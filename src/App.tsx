e8d4fe3ce2ef48e7fec83ea2a0d9ba3364cbc0dd
Anjali_Verma <verla003renu@gmail.com>
2025-07-14 14:21:32 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import Documents from "./pages/Documents";
+import Transaction from "./pages/Transaction";
+import Task from "./pages/Task";
+import Store from "./pages/Store";
+import Notification from "./pages/Notification";
+import Settings from "./pages/Setting";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/transaction" element={<Transaction />} />
+            <Route path="/task" element={<Task />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/notification" element={<Notification />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
